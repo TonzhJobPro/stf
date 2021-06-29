b73d80bd16952aae3b299bd1a08da62cc46edf77
root <root@localhost.localdomain>
2021-06-29 16:53:08 +0800

    Revert "ios dev 1"
    This reverts commit f90808dbbc762359830bf326c03ca40f69d6e48c.

--- restart.sh
+++
@@ -1,4 +0,0 @@
-# 默認 --vnc-initial-size 600x800 
-# adb kill-server && adb devices && rethinkdb --bind all --cache-size 8192 --http-port 7090
-cnpm install && kill -9 `ps -ef | grep 'stf\|iproxy\|tidevice' | grep -v grep|awk '{print $2}'` && npm link
-setsid stf local --public-ip 10.0.242.17 --allow-remote --port 7100 --auth-type ldap  --auth-options '["--ldap-url","ldaps://ldap.okg.com:636","--ldap-bind-dn","cn=readonly,dc=okcoin,dc=com","--ldap-search-dn","ou=People,dc=okcoin,dc=com","--ldap-bind-credentials","OKCoin_LDAP@readonly","--ldap-search-class","inetOrgPerson","--ldap-search-field","cn"]'
